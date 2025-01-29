22b77b79379cfc48ce73f83e4a7d92465870ee16
Mahnoor <mahnoort104@gmail.com>
2025-01-29 12:51:54 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['cdn.sanity.io'], // Add the external domain
+    },
+  };
+  
+  export default nextConfig;
+  
